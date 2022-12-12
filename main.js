142f1bd4a89a50b6a6ebccbfc5862da0e79d933e
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 09:50:11 -0600

    JS challenge with some functions that perform math problems.

---
+++ main.js
@@ -0,0 +1,31 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+let result = add(17, 9)
+console.log(result)
+
+const subtract = (firstNumber, secondNumber) => {
+    const difference = firstNumber - secondNumber
+    return difference
+}
+
+let subResult = subtract(12, 9)
+console.log(subResult);
+
+const divide = (firstNumber, secondNumber) => {
+    const quotient = firstNumber / secondNumber
+    return quotient
+}
+
+let divResult = divide(12, 4)
+console.log(divResult);
+
+const square = (number) => {
+    const skware = number * number
+    return skware
+}
+
+let squareResult = square(12)
+console.log(squareResult)
